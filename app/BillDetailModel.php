d3c2cc95a52319ba0fbc28d85b1a740026d6f1b6
Minh <hnim97@gmail.com>
2020-01-08 18:56:46 +0700

    t vừa đẩy nhé

--- app/BillDetailModel.php
+++ app/BillDetailModel.php
@@ -3,4 +3,6 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 class BillDetailModel extends Model{
     protected $table = 'bill_details';
-    protected $guarded = [''];}
+    protected $guarded = [''];
+    #Model bảng chi tiết sp 
+}
